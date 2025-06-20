2a630aa52a5de43a32fc2ef41df2fcd31a0046e9
jkszymczak <242548@edu.p.lodz.pl>
2025-06-20 18:47:48 +0200

    created func to retrive information from trait declaration

--- rusty_check/src/lib.rs
+++ rusty_check/src/lib.rs
@@ -2,19 +2,13 @@
 pub mod mocks {
     pub use mockall;
     pub use rusty_check_macros::rustymock;
-
-    struct Argument {
-        name: String,
-        t: String,
-    }
-    pub struct MethodDeclaration {
+    struct MethodDeclaration {
         declaration_literal: String,
-        arguments: Vec<Argument>,
-        return_type: String,
+        name: String,
+        args: String,
     }
     pub trait ComposableMock {
-        fn get_methods(&self) -> Vec<MethodDeclaration>;
-        fn compose<M, C>(&self, other: M) -> C;
+        fn get_methods(&self) -> Vec<String>;
     }
 }
 
--- rusty_check_macros/src/dsl/attribute_macros/automock.rs
+++ rusty_check_macros/src/dsl/attribute_macros/automock.rs
@@ -1,7 +1,9 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TS;
 use quote::format_ident;
 use quote::quote;
 use quote::ToTokens;
+use syn::FnArg;
 use syn::TraitItemFn;
 use syn::{parse_macro_input, ItemTrait, TraitItem};
 
@@ -17,17 +19,41 @@ pub fn automockfn(attr: TokenStream, item: TokenStream) -> TokenStream {
             _other => None,
         })
         .collect();
+    let method_declaration: Vec<TS> = functions
+        .iter()
+        .map(|f| {
+            let declaration_literal = f.sig.clone().into_token_stream().to_string();
+            let name = f.sig.ident.clone().to_string();
+            let args: Vec<String> = f
+                .sig
+                .inputs
+                .iter()
+                .filter_map(|arg| match arg {
+                    FnArg::Receiver(_) => Some("self".to_string()),
+                    FnArg::Typed(pat) => match &*pat.pat {
+                        syn::Pat::Ident(ident) => Some(ident.ident.to_string()),
+                        _ => None,
+                    },
+                })
+                .collect();
+            let args_str = args.join(", ");
+            quote! {
+                rusty_check::mocks::MethodDeclaration {
+                    declaration_literal: #declaration_literal,
+                    name: #name,
+                    args: #args_str
+                }
+            }
+            .into()
+        })
+        .collect();
     quote! {
         #[::mockall::automock]
         #trait_def
         impl rusty_check::mocks::ComposableMock for #mock_trait_name {
             fn get_methods(&self) -> Vec<rusty_check::mocks::MethodDeclaration> {
-                todo!();
+                vec![#(#method_declaration),*]
             }
-            fn compose<M,C>(&self, other: M) -> C {
-                todo!();
-            }
-
         }
     }
     .into()
